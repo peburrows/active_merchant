64dd3a3a306136d1a2f6433fe546ce16bc9e6ee0
Phil Burrows <peburrows@gmail.com>
2011-02-07 14:39:05 -0600

    that's not how I wanted to merge that commit

--- test/remote/gateways/remote_cyber_source_test.rb
+++ test/remote/gateways/remote_cyber_source_test.rb
@@ -8,11 +8,11 @@ class RemoteCyberSourceTest < Test::Unit::TestCase
 
     @credit_card = credit_card('4111111111111111', :type => 'visa')
     @declined_card = credit_card('801111111111111', :type => 'visa')
-    
+
     @amount = 100
-    
+
     @options = {
-      :billing_address => address,
+      :billing_address => address.merge(:first_name => 'Jim', :last_name => 'Smith'),
 
       :order_id => generate_unique_id,
       :line_items => [
@@ -29,15 +29,30 @@ class RemoteCyberSourceTest < Test::Unit::TestCase
           :description => 'Marble Snowcone',
           :sku => 'FAKE1232132113123'
         }
-      ],  
+      ],
       :currency => 'USD',
       :email => 'someguy1232@fakeemail.net',
       :ignore_avs => 'true',
       :ignore_cvv => 'true'
     }
 
+    @subscription_options = {
+      :order_id => generate_unique_id,
+      :email => 'someguy1232@fakeemail.net',
+      :credit_card => @credit_card,
+      :setup_fee => 100,
+      :billing_address => address.merge(:first_name => 'Jim', :last_name => 'Smith'),
+      :subscription => {
+        :frequency => "weekly",
+        :start_date => Date.today.next_week,
+        :occurrences => 4,
+        :auto_renew => true,
+        :amount => 100
+      }
+    }
+
   end
-  
+
   def test_successful_authorization
     assert response = @gateway.authorize(@amount, @credit_card, @options)
     assert_equal 'Successful transaction', response.message
@@ -63,18 +78,18 @@ class RemoteCyberSourceTest < Test::Unit::TestCase
   end
 
   def test_successful_tax_calculation_with_nexus
-    total_line_items_value = @options[:line_items].inject(0) do |sum, item| 
+    total_line_items_value = @options[:line_items].inject(0) do |sum, item|
                                sum += item[:declared_value] * item[:quantity]
                              end
-    
+
     canada_gst_rate = 0.05
     ontario_pst_rate = 0.08
-    
-    
+
+
     total_pst = total_line_items_value.to_f * ontario_pst_rate / 100
     total_gst = total_line_items_value.to_f * canada_gst_rate / 100
     total_tax = total_pst + total_gst
-    
+
     assert response = @gateway.calculate_tax(@credit_card, @options.merge(:nexus => 'ON'))
     assert_equal 'Successful transaction', response.message
     assert response.params['totalTaxAmount']
@@ -103,11 +118,11 @@ class RemoteCyberSourceTest < Test::Unit::TestCase
     assert auth = @gateway.authorize(@amount, @credit_card, @options)
     assert_success auth
     assert_equal 'Successful transaction', auth.message
-  
+
     assert capture = @gateway.capture(@amount, auth.authorization)
     assert_success capture
   end
-  
+
   def test_successful_authorization_and_failed_capture
     assert auth = @gateway.authorize(@amount, @credit_card, @options)
     assert_success auth
@@ -126,13 +141,11 @@ class RemoteCyberSourceTest < Test::Unit::TestCase
 
   def test_invalid_login
     gateway = CyberSourceGateway.new( :login => '', :password => '' )
-    authentication_exception = assert_raise ActiveMerchant::ResponseError, 'Failed with 500 Internal Server Error' do
-      gateway.purchase(@amount, @credit_card, @options)
+    assert_raises ActiveMerchant::ResponseError do
+      assert response = gateway.purchase(@amount, @credit_card, @options)
     end
-    assert response = authentication_exception.response
-    assert_match /wsse:InvalidSecurity/, response.body
   end
-  
+
   def test_successful_credit
     assert response = @gateway.purchase(@amount, @credit_card, @options)
     assert_equal 'Successful transaction', response.message
@@ -141,6 +154,50 @@ class RemoteCyberSourceTest < Test::Unit::TestCase
     assert response = @gateway.credit(@amount, response.authorization)
     assert_equal 'Successful transaction', response.message
     assert_success response
-    assert response.test?       
+    assert response.test?
+  end
+
+  def test_successful_create_subscription
+    assert response = @gateway.create_subscription(@credit_card, @subscription_options)
+    assert_equal 'Successful transaction', response.message
+    assert_success response
+    assert response.test?
+  end
+
+  def test_successful_update_subscription
+    assert response = @gateway.create_subscription(@credit_card, @subscription_options)
+    assert_equal 'Successful transaction', response.message
+    assert_success response
+    assert response.test?
+
+    assert response = @gateway.update_subscription(response.authorization, @subscription_options.merge(:subscription => {:amount => 500}))
+    assert_equal 'Successful transaction', response.message
+    assert_success response
+    assert response.test?
+  end
+
+  def test_successful_subscription_purchase
+    assert response = @gateway.create_subscription(@credit_card, @subscription_options)
+    assert_equal 'Successful transaction', response.message
+    assert_success response
+    assert response.test?
+
+    assert response = @gateway.subscription_purchase(@amount, response.authorization, @options)
+    assert_equal 'Successful transaction', response.message
+    assert_success response
+    assert response.test?
+  end
+
+  def test_successful_subscription_retrieval
+    assert response = @gateway.create_subscription(@credit_card, @subscription_options)
+    assert_equal 'Successful transaction', response.message
+    assert_success response
+    assert response.test?
+
+    assert response = @gateway.retrieve_subscription(response.authorization, @options)
+    assert_equal 'Successful transaction', response.message
+    assert_success response
+    assert response.test?
+>>>>>>> 8890f19... added profile management to CyberSource gateway
   end
 end
